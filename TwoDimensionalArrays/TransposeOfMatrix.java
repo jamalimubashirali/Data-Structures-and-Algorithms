bd7983f05d0be8bba7076f6ab06492562ab1cbca
Mubashir <21sw071@students.muet.edu.pk>
2022-12-18 00:02:19 +0500

    sorting a two-dimensional array

--- TwoDimensionalArrays/TransposeOfMatrix.java
+++ TwoDimensionalArrays/TransposeOfMatrix.java
@@ -2,7 +2,7 @@ package TwoDimensionalArrays;
 
 import java.util.Scanner;
 
-public class SpiralOrderMatrix {
+public class TransposeOfMatrix {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int rows,cols;
@@ -17,10 +17,11 @@ public class SpiralOrderMatrix {
                 array[i][j] = sc.nextInt();
             }
         }
-        for (int i = 0 ; i < rows ; i++){
-            for (int j = 0 ; j < cols ; j++){
+        for (int i = 0 ; i < cols ; i++){
+            for (int j = 0 ; j < rows ; j++){
                 System.out.print(array[j][i]+" ");
             }
+            System.out.println();
         }
     }
 }
