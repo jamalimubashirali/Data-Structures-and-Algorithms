c2776bcd23b008eeb1101f3ce737e26821e3ea8d
Mubashir <21sw071@students.muet.edu.pk>
2022-12-17 23:52:42 +0500

    sorting a two-dimensional array

---
+++ TwoDimensionalArrays/SpiralOrderMatrix.java
@@ -0,0 +1,26 @@
+package TwoDimensionalArrays;
+
+import java.util.Scanner;
+
+public class SpiralOrderMatrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rows,cols;
+        System.out.println("Enter number of rows :");
+        rows = sc.nextInt();
+        System.out.println("Enter number of columns :");
+        cols = sc.nextInt();
+        int[][] array = new int[rows][cols];
+        for(int i = 0; i<rows; i++){
+            System.out.println("Enter numbers in row :"+i);
+            for (int j = 0; j<cols ; j++){
+                array[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0 ; i < rows ; i++){
+            for (int j = 0 ; j < cols ; j++){
+                System.out.print(array[i][j]+" ");
+            }
+        }
+    }
+}
