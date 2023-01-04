12e218fc61443bed070775e6cb7e23f95967cc0c
Mubashir <21sw071@students.muet.edu.pk>
2023-01-04 23:02:58 +0500

    Linked List in java

--- LinkedList_And_Types/LinkedList.java
+++ LinkedList_And_Types/LinkedList.java
@@ -14,6 +14,41 @@ public class LinkedList {
             }
             System.out.print("Null");
         }
+        public Node insertNode(Node start,Node newNode){
+            if(start == null)
+                return start;
+            if(start.data > newNode.data){
+                newNode.next =start;
+                start = newNode;
+                return start;
+            }
+            for (Node p = start ; p!= null; p = p.next){
+                if(p.next == null){
+                    p.next = newNode;
+                    newNode.next = null;
+                }
+            }
+            return start;
+        }
+        public Node deleteNode(Node start,Node deleteNode){
+            if(start == null)
+                return start;
+            if(start.next == null){
+                start = null;
+                return start;
+            }
+            Node p;
+            for (p = start; p.next!= null; p = p.next){
+                if(p.next.next== null && p.next.data == deleteNode.data){
+                    p.next = null;
+                    return start;
+                }
+               if(deleteNode.data == p.next.data){
+                    p.next = p.next.next;
+                }
+            }
+            return start;
+        }
     }
     public static void main(String[] args) {
        Node start = new Node(1);
@@ -22,6 +57,7 @@ public class LinkedList {
            p.next = new Node(i);
            p = p.next;
         }
+       start = start.deleteNode(start,new Node(9));
        start.printList(start);
     }
 }
