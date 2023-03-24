34ef70f91b5e4c825147601fe7bddc09868734a4
Mubashir <21sw071@students.muet.edu.pk>
2023-03-24 17:05:17 +0500

    Added Rebalnace Method and Created BST class along with its methods

--- BinaryTrees/BinarySearchTree.java
+++ BinaryTrees/BinarySearchTree.java
@@ -1,22 +1,22 @@
 package BinaryTrees;
 
-public class AVLTree {
+public class BinarySearchTree {
     private int key,height;
     private Object value;
-    private AVLTree left,right;
-    public static final AVLTree NIL = new AVLTree();
-    private AVLTree(){
+    private BinarySearchTree left,right;
+    public static final BinarySearchTree NIL = new BinarySearchTree();
+    private BinarySearchTree(){
         left = right = this;
         height = -1;
     }
-    private AVLTree(int key, AVLTree left, AVLTree right,Object value){
+    private BinarySearchTree(int key, BinarySearchTree left, BinarySearchTree right,Object value){
         this.key = key;
         this.value = value;
         this.right = right;
         this.left = left;
         this.height = 1+Math.max(left.height,right.height);
     }
-    public AVLTree(int key,Object value){
+    public BinarySearchTree(int key,Object value){
         this.key = key;
         this.value = value;
         this.left = this.right = NIL;
@@ -26,16 +26,15 @@ public class AVLTree {
             return 0;
         return 1+ left.size()+ right.size();
     }
-    public AVLTree grow(int key,Object value){
+    public BinarySearchTree grow(int key,Object value){
         if(this == NIL)
-            return new AVLTree(key,value);
+            return new BinarySearchTree(key,value);
         if(key == this.key)
             return this;
         if(key < this.key)
             left = left.grow(key,value);
         if(key > this.key)
             right = right.grow(key,value);
-        rebalance();
         height = 1+Math.max(left.height, right.height);
         return this;
     }
@@ -52,29 +51,6 @@ public class AVLTree {
             return;
         }
         this.right.rightMostValue();
-    }
-    private void rotateRight(){
-        this.right = new AVLTree(this.key,this.value);
-        this.key = this.left.right.key;
-        this.value = this.left.right.value;
-        this.left.right = NIL;
-    }
-    private void rotateLeft(){
-            this.left = new AVLTree(this.key,this.value);
-            this.key = this.right.left.key;
-            this.value = this.right.left.value;
-            this.right.left = NIL;
-    }
-    private void rebalance(){
-        if(this.right.height > this.left.height+1){
-            if(this.right.left.height > right.right.height)
-                this.right.rotateRight();
-            this.rotateLeft();
-        } else if (this.left.height > this.right.height+1) {
-            if(this.left.right.height > this.left.left.height)
-                this.left.rotateLeft();
-            this.rotateRight();
-        }
     }
     public boolean add(int key,Object value){
         int oldSize = size();
