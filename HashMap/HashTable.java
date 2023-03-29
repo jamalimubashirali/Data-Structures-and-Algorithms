ddc97574cfa413e3cf02cd69d11987054d4ea210
Mubashir <21sw071@students.muet.edu.pk>
2023-03-29 22:30:19 +0500

    Created HashTable added more method like probingHandler

--- HashMap/HashTable.java
+++ HashMap/HashTable.java
@@ -21,8 +21,6 @@ public class HashTable {
         int h = hash(key);
         for (int i = 0; i < nodes.length; i++) {
             int j = (h + i) % nodes.length;
-//            if(nodes[j] == null)
-//                break;
             if (nodes[j] != null && nodes[j].key.equals(key)) {
                 return nodes[j].value;
             }
@@ -42,8 +40,7 @@ public class HashTable {
         int h = hash(key);
         for(int i = 0; i < nodes.length; i++){
             int j = (h+i) % nodes.length;
-            Node tempNode = nodes[j];
-            if(tempNode == null){
+            if(nodes[j] == null){
                 nodes[j] = new Node(key, value);
                 size++;
                 return;
