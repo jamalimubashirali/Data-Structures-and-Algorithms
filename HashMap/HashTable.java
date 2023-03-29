1d00d8191ad0e6338b5ef863d40e197fc080beb5
Mubashir <21sw071@students.muet.edu.pk>
2023-03-29 22:48:12 +0500

    Created HashTable added more method like probingHandler

--- HashMap/HashTable.java
+++ HashMap/HashTable.java
@@ -21,6 +21,8 @@ public class HashTable {
         int h = hash(key);
         for (int i = 0; i < nodes.length; i++) {
             int j = (h + i) % nodes.length;
+//            if(nodes[j] == null)
+//                break;
             if (nodes[j] != null && nodes[j].key.equals(key)) {
                 return nodes[j].value;
             }
@@ -37,6 +39,8 @@ public class HashTable {
         size++;
     }
     public void putBylinearProbing(Object key,Object value){
+        if(size == nodes.length)
+            this.reHash();
         int h = hash(key);
         for(int i = 0; i < nodes.length; i++){
             int j = (h+i) % nodes.length;
@@ -85,7 +89,7 @@ public class HashTable {
                     if (nodes[k] == null) {
                         nodes[k] = oldNodes[i];
                         size++;
-                        return;
+                        break;
                     }
                 }
             }
